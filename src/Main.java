66dd332c83f98a868fd9a9610113d499ae53dc64
AleksandrLiagushin <a.lyagushin@yandex.ru>
2023-04-16 15:38:46 +0530

    Version 1.3. Added global vars to main

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,18 @@
 import java.util.Scanner;
 /*
 * Program StepTracker. Storing, editing, targeting and steps statistics for each month
-* @version 1.0 13/04/2023
+* @version 1.1 13/04/2023
 * @author Aleksandr Liagushin
 */
 public class Main {
 
+    protected static final int ADD_STEPS = 1;
+    protected static final int CHANGE_TARGET = 2;
+    protected static final int CALL_CONVERTER = 3;
+    protected static final int PRINT_STAT = 4;
+    protected static final int QUIT = 0;
+    protected static final int CONVERT_TO_KM = 1;
+    protected static final int CONVERT_TO_KKAL = 2;
     public static void main(String[] args) {
         Scanner userInput = new Scanner(System.in);
         StepTracker stepTracker = new StepTracker(userInput);
@@ -15,27 +22,27 @@ public class Main {
         while (true) {
             Navigator.printMenu();
             menuPoint = userInput.nextInt();
-            if (menuPoint == 1) {
+            if (menuPoint == ADD_STEPS) {
                 stepTracker.addNewNumberStepsPerDay();
                 System.out.println();
-            } else if (menuPoint == 2) {
+            } else if (menuPoint == CHANGE_TARGET) {
                 stepTracker.changeStepGoal();
                 System.out.println();
-            } else if (menuPoint == 3) {
+            } else if (menuPoint == CALL_CONVERTER) {
                 Navigator.printConverterMenu();
                 menuPoint = userInput.nextInt();
                 System.out.println();
-                if (menuPoint == 1) {
+                if (menuPoint == CONVERT_TO_KM) {
                     monthData = Navigator.getMonthData(userInput, stepTracker);
                     System.out.printf("В этом месяце вы покрыли дистанцию в %.2f км.\n",
                             (Converter.convertToKm(MonthData.sumStepsFromMonth(monthData))));
                     System.out.println();
-                } else if (menuPoint == 2) {
+                } else if (menuPoint == CONVERT_TO_KKAL) {
                     monthData = Navigator.getMonthData(userInput, stepTracker);
                     System.out.println("В этом месяце вы сожгли: "
                             + Converter.convertStepsToKilocalories(MonthData.sumStepsFromMonth(monthData)) + " кКал.");
                     System.out.println();
-                } else if (menuPoint == 0) {
+                } else if (menuPoint == QUIT) {
                     System.out.println("Вы напралены в основное меню.");
                     System.out.println();
                 } else {
@@ -43,13 +50,14 @@ public class Main {
                     System.out.println("Вы напралены в основное меню.");
                     System.out.println();
                 }
-            } else if (menuPoint == 4) {
+            } else if (menuPoint == PRINT_STAT) {
                 monthData = Navigator.getMonthData(userInput, stepTracker);
                 Converter.printStatistic(stepTracker, monthData);
                 System.out.println();
-            } else if (menuPoint == 0) {
+            } else if (menuPoint == QUIT) {
                 System.out.println("Хорошего дня и достижения поставленных целей!");
                 System.out.println();
+                userInput.close();
                 return;
             } else {
                 System.out.println("Такого пункта меню нет.");
